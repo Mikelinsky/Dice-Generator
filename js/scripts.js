b09a6c435555f9482c705f6db2727da95d2cb30a
Michał Kupiecki <michal481216@gmail.com>
2018-06-18 21:30:49 +0200

    add js code

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,42 @@
 var rollButton = document.getElementById("js-rollButton");
-var numberOfRoll = document.getElementById("js-numberOfRoll");
+var result = document.getElementById("js-result");
+var dice = document.getElementsByClassName("dice");
+
 
 rollButton.addEventListener("click", roll);
+
+function roll() {
+    var inputElem = document.getElementById("js-numberOfRoll").value;
+    
+    //if (typeof inputElem == "string") {}
+
+    for (var i = 0; i <= dice.length; i++) {
+        if (dice[i].checked === true) {
+            //dice[i].value;
+            console.log(dice[i].value);
+            console.log(dice[i].checked);
+            break;
+        } 
+    }
+
+    if (dice[i].value == 1) {
+        console.log("mamy 1");
+    } else if (dice[i].value == 2) {
+        console.log("mamy 2");
+    } else if (dice[i].value == 3) {
+        console.log("mamy 3");
+    } else if (dice[i].value == 4) {
+        console.log("mamy 4");
+    } else if (dice[i].value == 5) {
+        console.log("mamy 5");
+    } else if (dice[i].value == 6) {
+        console.log("mamy 6");
+    }
+    
+
+    result.innerHTML = "";
+    result.innerHTML = inputElem;
+    
+}
+
+
