03294757a2488a1d8a59d63fa8b625e7a93f68dd
Michał Kupiecki <michal481216@gmail.com>
2018-06-19 16:14:14 +0200

    add functioning js

--- js/scripts.js
+++ js/scripts.js
@@ -1,42 +1,60 @@
 var rollButton = document.getElementById("js-rollButton");
 var result = document.getElementById("js-result");
 var dice = document.getElementsByClassName("dice");
-
+var bankForRolls = [];
 
 rollButton.addEventListener("click", roll);
 
 function roll() {
     var inputElem = document.getElementById("js-numberOfRoll").value;
-    
-    //if (typeof inputElem == "string") {}
 
     for (var i = 0; i <= dice.length; i++) {
         if (dice[i].checked === true) {
-            //dice[i].value;
-            console.log(dice[i].value);
-            console.log(dice[i].checked);
+            dice[i].value;
             break;
         } 
     }
-
-    if (dice[i].value == 1) {
-        console.log("mamy 1");
-    } else if (dice[i].value == 2) {
-        console.log("mamy 2");
-    } else if (dice[i].value == 3) {
-        console.log("mamy 3");
-    } else if (dice[i].value == 4) {
-        console.log("mamy 4");
-    } else if (dice[i].value == 5) {
-        console.log("mamy 5");
-    } else if (dice[i].value == 6) {
-        console.log("mamy 6");
-    }
     
-
     result.innerHTML = "";
-    result.innerHTML = inputElem;
+    bankForRolls = [];
     
+    if (dice[i].value == 1) {
+        for (var j = 0; j < inputElem; j++) {
+            var d4 = Math.floor(Math.random() * 4) + 1;
+            bankForRolls.push(d4);
+            console.log("d4 is rolling");
+        }
+    } else if (dice[i].value == 2) {
+        for (var j = 0; j < inputElem; j++) {
+            var d6 = Math.floor(Math.random() * 6) + 1;
+            bankForRolls.push(d6);
+            console.log("d6 is rolling");
+        }
+    } else if (dice[i].value == 3) {
+        for (var j = 0; j < inputElem; j++) {
+            var d8 = Math.floor(Math.random() * 8) + 1;
+            bankForRolls.push(d8);
+            console.log("d8 is rolling");
+        }
+    } else if (dice[i].value == 4) {
+        for (var j = 0; j < inputElem; j++) {
+            var d10 = Math.floor(Math.random() * 10) + 1;
+            bankForRolls.push(d10);
+            console.log("d10 is rolling");
+        }
+    } else if (dice[i].value == 5) {
+        for (var j = 0; j < inputElem; j++) {
+            var d12 = Math.floor(Math.random() * 12) + 1;
+            bankForRolls.push(d12);
+            console.log("d12 is rolling");
+        }
+    } else if (dice[i].value == 6) {
+        for (var j = 0; j < inputElem; j++) {
+            var d20 = Math.floor(Math.random() * 20) + 1;
+            bankForRolls.push(d20);
+            console.log("d20 is rolling");
+        }
+    } 
+    
+    console.log(bankForRolls);
 }
-
-
